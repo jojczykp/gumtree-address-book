5f2ad2cc825c6819155e5776e70497e4370cc584
mrJojo <pawel.jojczyk@gmail.com>
2016-03-11 20:43:09 +0100

    Basic run

--- src/main/java/pl/jojczykp/gumtree/address_book/Application.java
+++ src/main/java/pl/jojczykp/gumtree/address_book/Application.java
@@ -2,8 +2,25 @@ package pl.jojczykp.gumtree.address_book;
 
 public class Application {
 
+	private void run(String[] args) {
+		if (args.length != 1) {
+			printUsage();
+		} else {
+			process(args[0]);
+		}
+	}
+
+	private void printUsage() {
+		System.err.println("Error: Wrong number of arguments.");
+		System.err.println("Usage: java -jar gumtree-address-book.jar <address book file name>");
+	}
+
+	private void process(String pathToFile) {
+	}
+
 	public static void main(String args[]) {
-		System.out.println("Hello World!");
+		Application application = new Application();
+		application.run(args);
 	}
 
 }
